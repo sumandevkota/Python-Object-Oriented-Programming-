b6c83ff56b9b2b6a3511df3db9afab36312da94d
Suman Devkota <37627609+sumandevkota@users.noreply.github.com>
2022-09-10 13:26:52 -0400

    Add files via upload

---
+++ Ex_Files_Python_Object_Programming/Exercise Files/Ch 1/instance_start.py
@@ -0,0 +1,44 @@
+# Python Object Oriented Programming by Joe Marini course example
+# Using instance methods and attributes
+
+
+
+    # the "init" function is called when the instance is
+    # created and ready to be initialized
+class  Book:
+        def __init__(self,title,author,pages,price):
+            
+           self.title = title
+         # TODO: add properties
+           self.author = author
+           self.pages = pages
+           self.price = price
+           self.__secret = "This is a secret attribute"
+
+    # TODO: create instance methods
+        def getprice(self):
+            if hasattr(self,"_discount"):
+                return self.price - (self.price *self._discount)
+            else:
+                return self.price
+                 
+     
+     
+        def setdiscount(self,amount):
+            self._discount = amount
+
+# TODO: create some book instances
+b1 = Book("War and Peace","Leo Tolstoy",1225,39.95)
+b2 = Book("The catcher in the Rye","JD salinger",234,29.95)
+
+# TODO: print the price of book1
+print(b1.getprice())
+
+
+# TODO: try setting the discount
+print(b2.getprice())
+b2.setdiscount(0.25)
+print(b2.getprice())
+
+# TODO: properties with double underscores are hidden by the interpreter
+print(b2._Book__secret)
---
+++ Ex_Files_Python_Object_Programming/Exercise Files/Ch 2/inheritance_start.py
@@ -0,0 +1,44 @@
+# Python Object Oriented Programming by Joe Marini course example
+# Understanding class inheritance
+class Publication:
+    def __init__(self,title,price):
+        self.title = title
+        self.price = price
+        
+        
+class periodical(Publication):
+    def __init__(self,title,price,period,publisher):
+        super().init(title,price)
+        self.period = period
+        self.publisher = publisher
+        
+class Book(Publication):
+    def __init__(self, title, author, pages, price):
+        super().__init__(title,price)
+        self.author = author
+        self.pages = pages
+
+
+class Magazine:
+    def __init__(self, title, publisher, price, period):
+        super().init(title,price,period,publisher)
+        #self.price = price
+        #self.period = period
+        #self.publisher = publisher
+
+
+class Newspaper:
+    def __init__(self, title, publisher, price, period):
+        super().init(title,price,period,publisher)
+        #self.price = price
+        #self.period = period
+        #self.publisher = publisher
+
+
+b1 = Book("Brave New World", "Aldous Huxley", 311, 29.0)
+n1 = Newspaper("NY Times", "New York Times Company", 6.0, "Daily")
+m1 = Magazine("Scientific American", "Springer Nature", 5.99, "Monthly")
+
+print(b1.author)
+print(n1.publisher)
+print(b1.price, m1.price, n1.price)
